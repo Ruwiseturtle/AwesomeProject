3cb543920cee6b513e9c1ed9fb65a7c146c7fe45
Matviienko Ruslana <brauniesrus@gmail.com>
2024-03-18 00:46:42 +0200

    ssssssss

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>bla bla bla bla</Text>
+      <Text>bla bla bla dddddddddddddddbla</Text>
       <StatusBar style="auto" />
     </View>
   );
