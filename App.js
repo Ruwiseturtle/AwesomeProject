f5365b49ace970a0083fee513503e36f18302c20
Matviienko Ruslana <brauniesrus@gmail.com>
2024-03-18 18:53:26 +0200

    env

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>bla bla bla dddddddddddddddbla</Text>
+      <Text>bla bla bla 123</Text>
       <StatusBar style="auto" />
     </View>
   );
