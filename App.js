da976b2381e0339a6b5d36e45cb3ecdcaa6ff0d7
Matviienko Ruslana <brauniesrus@gmail.com>
2024-03-21 00:08:02 +0200

    backgroundColor

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Image, View } from "react-native";
 import { useFonts } from "expo-font";
+import { RegistrationScreen } from "./Screens/RegistrationScreen";
 
 export default function App() {
-    const [fontsLoaded] = useFonts({
-      "Inter-Black": require("./assets/fonts/inter-black.ttf"),
-    });
-  
-   if (!fontsLoaded) {
-     return null;
-   }
-  
+  const [fontsLoaded] = useFonts({
+    "Inter-Black": require("./assets/fonts/inter-black.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>bla bla bla bla</Text>
-      <Text style={styles.text}>bla bla bla 123</Text>
+      <Image
+        source={require("./assets/Photo-BG.jpeg")}
+        style={styles.backgroundImage}
+      />
+      <RegistrationScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -23,9 +27,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "white",
     alignItems: "center",
     justifyContent: "center",
+  },
+  backgroundImage: {
+    width: "100%",
+    height: "100%",
   },
   text: {
     color: "black",
--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-const RegistrationScreen = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+export const RegistrationScreen = () => {
+  return (    
+      <Text style={styles.text}>сторінка реєстрації</Text>   
+  );
+};
 
-export default RegistrationScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center", // Вирівнювання тексту по центру по горизонталі
+    justifyContent: "center", // Вирівнювання тексту по центру по вертикалі
+    position: "relative",
+  },
+  text: {
+    color: "white",
+    fontSize: 30,
+    fontWeight: "bold",
+    position: "absolute",
+  },
+});
