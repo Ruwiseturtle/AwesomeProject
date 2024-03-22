89b11903fe1f76ef546c1e567a9707128309dacd
Matviienko Ruslana <brauniesrus@gmail.com>
2024-03-22 21:15:19 +0200

    added postscreen

---
+++ Screens/PostsScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyleSheet, Text, View } from "react-native";
+
+const PostsScreen = () => {
+  return (
+    <View>
+      <Text>PostsScreen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default PostsScreen
